bf136000d8e40efdfc526b7398c1bc6b58ce263f
FaithlessLooting <matthew.dove13@gmail.com>
2021-01-26 20:46:47 +0000

    testing auth
    On reflection this system for authentication with admin roles seems a little overcomplecated, final commit to this branch, try again with fresh project

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,4 +1,4 @@
-@extends('layout')
+@extends('layouts.app')
 @section('content')
 
 <h1>Showing: {{ $article->topic }}</h1>
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,20 @@ Route::get('/', function () {
 });
 
 Route::resource('articles', 'ArticleController');
+
+Auth::routes();
+
+Route::get('/login/admin', 'Auth\LoginController@showAdminLoginForm');
+Route::get('/login/writer', 'Auth\LoginController@showWriterLoginForm');
+Route::get('/register/admin', 'Auth\RegisterController@showAdminRegisterForm');
+Route::get('/register/writer', 'Auth\RegisterController@showWriterRegisterForm');
+
+Route::post('/login/admin', 'Auth\LoginController@adminLogin');
+Route::post('/login/writer', 'Auth\LoginController@writerLogin');
+Route::post('/register/admin', 'Auth\RegisterController@createAdmin');
+Route::post('/register/writer', 'Auth\RegisterController@createWriter');
+
+
+Route::view('/home', 'home')->middleware('auth');
+Route::view('/admin', 'admin');
+Route::view('/writer', 'writer');
